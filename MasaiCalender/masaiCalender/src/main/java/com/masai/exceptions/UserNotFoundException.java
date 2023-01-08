f47ad40fd5a2ce68430e71ccbfd6c7da97f59e7b
Pritu2022 <candlepritu21century@gmail.com>
2023-01-08 15:51:17 +0530

    added

--- MasaiCalender/masaiCalender/src/main/java/com/masai/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.masai.exceptions;
-
-public class UserNotFoundException extends Exception{
-        public UserNotFoundException(String s) {
-			super(s);
-		}
-}
---
+++ springbootsampleapp1/src/main/java/com/masai/app/Springbootsampleapp1Application.java
@@ -0,0 +1,13 @@
+package com.masai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootsampleapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootsampleapp1Application.class, args);
+	}
+
+}
